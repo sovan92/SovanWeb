8932855adfe5a2318f4f639e4e88072e2026a3ca
Sovan Rath <sovanrath@sovans-mbp-2.lan>
2022-01-15 10:23:58 -0800

    initial commit

---
+++ src/main/java/com/example/sovanweb/SovanWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.sovanweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SovanWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SovanWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sovanweb/SovanWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sovanweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SovanWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
